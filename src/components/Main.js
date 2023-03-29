f760fa80e633f8354bfd510ef7ed6c1e011b692e
Toto <19580671+tomso11@users.noreply.github.com>
2023-03-28 22:59:06 -0300

    start project and add navbar

---
+++ src/components/Main.js
@@ -0,0 +1,9 @@
+const Main = () => {
+    return(
+        <div className="main">
+            Main goes here
+        </div>
+    )
+}
+
+export default Main
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+
+const Navbar = () => {
+    return(
+        <div className="navbar">
+            <img className="react-logo" src="./logo192.png" alt="small react icon"></img>
+            <h1 className="page-title">ReactFacts</h1>
+            <p className="page-description">React Course - Project 1</p>
+        </div>
+    )
+}
+
+export default Navbar
