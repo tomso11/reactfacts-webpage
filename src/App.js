2f2a9f8fac8022577a87db7d501cddef3c675c3a
Toto <19580671+tomso11@users.noreply.github.com>
2023-04-22 22:25:56 -0300

    add darkmode

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './index.css';
+import React, { useState } from 'react';
 import Navbar from './components/Navbar';
 import Main from './components/Main';
 
 function App() {
+
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const handleDarkModeToggle = () => {
+    console.log("Dark mode toggled!");
+    setIsDarkMode(!isDarkMode);
+  };
+
+
   return (
-    <div>
-      <Navbar/>
+    <div className={`App ${isDarkMode ? "dark" : "light"}`}>
+      <Navbar
+        isDarkMode={isDarkMode}
+        handleDarkModeToggle={handleDarkModeToggle}
+      />
       <Main/>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,41 @@
+import React, { useEffect } from 'react';
 
-const Navbar = () => {
+const Navbar = (props) => {
+
+    useEffect(() => {
+        const body = document.querySelector('body');
+        if (props.isDarkMode) {
+            body.classList.add('dark');
+        } else {
+            body.classList.remove('dark');
+        }
+    }, [props.isDarkMode]);
+
+
     return(
         <nav className="navbar">
             <img className="react-logo" src="./logo192.png" alt="small react icon"></img>
             <h3 className="page-title">ReactFacts</h3>
-            <h4 className="page-description">React Course - Project 1</h4>
+
+            <div className="switch-container">
+                {props.isDarkMode ? (
+                    <span className="label light-mode-label dark">Light</span>
+                    ) : (
+                    <span className="label light-mode-label">Light</span>
+                    )
+                }
+                
+                <label className="switch">
+                <input type="checkbox" checked={props.isDarkMode} onChange={props.handleDarkModeToggle} />
+                <span className="slider"></span>
+                </label>
+                {props.isDarkMode ? (
+                    <span className="label dark-mode-label dark">Dark</span>
+                    ) : (
+                    <span className="label dark-mode-label">Dark</span>
+                    )
+                }
+            </div>
         </nav>
     )
 }
